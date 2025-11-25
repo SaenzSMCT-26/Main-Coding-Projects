e0a0ebaacd0fd5ebe6926dc79f74cee1104e054e
saenz-smct-26 <saenz.smct.26@gmail.com>
2025-11-24 20:36:13 -0600

    worked on the long/short bias message display

--- Main Projects/script.js
+++ Main Projects/script.js
@@ -4,52 +4,65 @@
 // const short = document.querySelector('.short_value');
 // const changeInLong = document.querySelector('.long_value_change');
 // const changeInShort = document.querySelector('.short_value_change');
-const long = 200000;
-const short = 45000;
+const long = 190000;
+const short = 200000;
 const changeInLong = 19888;
 const changeInShort = -3000;
 
 /* we can create constant variables to hold the percentage values for compared_result */
-const [lp1, lp2, lp3, lp4] = [(long * 0.25), (long * 0.50), (long * 0.75), (long * 1)];
-const 
+const [lp1, lp2, lp3] = [(long * 0.25), (long * 0.50), (long * 0.75)];
+const [sp1, sp2, sp3] = [(short * 0.25), (short * 0.50), (short * 0.75)];
 
 function position_comparing(long, short) {
 
-      let compared_result;
+      let bias;
 
-      if(short <= lp1) {
-            compared_result = 8.75 * 4;//extremely bullish
-      } else if (short <= lp2 && short > lp1)
+      if (short === long) {
+            bias = "neatral";
+      } else if (short <= lp1) {
+            bias  = "extremely bullish";
+      } else if (short <= lp2 && short > lp1) {
+            bias  = "very bullish";
+      } else if (short <= lp3 && short > lp2) {
+            bias  = "bullish";
+      } else if (long <= sp1) {
+            bias = "extremely bearish";
+      } else if (long <= sp2 && long > lp1) {
+            bias = "very bearish";
+      } else if (long <= sp3 && long > lp2) {
+            bias = "bearish";
+      } else bias = "too close for consideration"
 
-return compared_result;
+      return bias;
 }
 
+console.log(position_comparing(long, short));
 // const array = [0.70, 0.25, -0.05];
 /* -------------- calculation function object -------------- */
-const calculation_block = {
+/* const calculation_block = {
       subtraction: function(long_value, short_value) {
             const sub = long_value - short_value;
             return sub;
       },
-}
+} */
 
 /* console.log(calc); */
 
-const bullish_sentiment = 100;
-function commitments_of_traders_data() {
+// const bullish_sentiment = 100;
+/* function commitments_of_traders_data() {
 
 
-      /* this is the current net position or the current amount of contracts the non-commercial are holding on to */
+      //this is the current net position or the current amount of contracts the non-commercial are holding on to
       const current_net_position = calculation_block.subtraction(Number(long), Number(short));
 
-      /* this is the amount of contracts added or offloaded to and from the current net position */
+      //this is the amount of contracts added or offloaded to and from the current net position
       const change_in_net = calculation_block.subtraction(Number(changeInLong), Number(changeInShort));
 
-      /*This will be the net position minus the change in net*/
+      //This will be the net position minus the change in net
       const previous = calculation_block.subtraction(Number(current_net_position), Number(change_in_net));
 
       
-      /* return current_net_position ;//TEST */
+      return current_net_position ;//TEST
 
 
 
@@ -66,11 +79,11 @@ function commitments_of_traders_data() {
       // }
 
 
-}
+} */
 
-const result = commitments_of_traders_data();
+// const result = commitments_of_traders_data();
 
-console.log(result);
+// console.log(result);
 
 // document.querySelector('#calculate').addEventListener('click', () => {
 //       // const calc = calculation_block.weighted_score_calc(array[0], array[1], array[2]);
