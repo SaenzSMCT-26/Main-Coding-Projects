5d9fa92eca5e348cf3cec8d7ca990d63cf8fb5e0
SaenzSMCT-26 <saenz.smct.26@gmail.com>
2025-12-14 21:20:51 -0600

    commit

---
+++ Margin Calculator/script.js
@@ -0,0 +1,13 @@
+"strict mode";
+/* displays the final result which will be the total dollar amount that will be required at all times for a specific currency pair */
+const display = document.querySelector('#display');
+
+/* this is where you are going to enter your current lot size you want to use for a trade position */
+const lotSize = document.querySelector('#lot_size');
+
+/* This is the percentage of margin that is required at the most for a specified currency pair */
+const marginPercentage = document.querySelector('#margin_percent');
+
+
+
+const marginCalc()
